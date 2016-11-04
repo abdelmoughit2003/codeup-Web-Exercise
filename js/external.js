2d0eb1dbdaa563df952b9fe3d5a85113c3b26b7e
abdou <laajali.abdelmoughit@gmail.com>
2016-11-03 19:58:54 -0500

    i delete some files

---
+++ js/external.js
@@ -0,0 +1,7 @@
+/**
+ * Created by sim on 11/3/16.
+ */
+"use strict";
+
+
+console.log('Hello from JavaScript!');
---
+++ js/if-else.js
@@ -0,0 +1,46 @@
+var flipACoin = Math.floor(Math.random()* 2)
+var avg = (70+80+95)/3;
+if(avg > 80){
+    console.log("you're awesome")
+}else{
+    console.log("you need to practice more.")
+}
+
+var price = 200;
+var name1 = 'Ryan';
+var name2 = 'Cameron';
+var name3 = 'George';
+var amountbeforefdiscount1 = 180;
+var amountbeforefdiscount2 = 250;
+var amountbeforefdiscount3 = 320;
+var discount = 100;
+
+
+if(amountbeforefdiscount1>price){
+    amountafterdiscount = amountbeforefdiscount1-discount
+    console.log( name1 + " amount before discount " + amountbeforefdiscount1 + " amount after discount " + amountafterdiscount)
+
+}
+else{
+    console.log(name1 + " No discount")
+}
+if(amountbeforefdiscount2>price){
+    amountafterdiscount = amountbeforefdiscount2-discount
+    console.log( name2 + " amount before discount " + amountbeforefdiscount2 + " amount after discount " + amountafterdiscount)
+}
+else{
+    console.log(name2 + " No discount ")
+}
+
+    if(amountbeforefdiscount3>price){
+        amountafterdiscount = amountbeforefdiscount3-discount
+        console.log( name3 + " amount before discount " + amountbeforefdiscount3 + " amount after discount " + amountafterdiscount)
+
+    }
+    else{
+        console.log(name3 + " No discount")
+    }
+
+
+
+ (flipACoin===0)? console.log(" Buy a car"): console.log(" Buy a house");
